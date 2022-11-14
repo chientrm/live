73f6e6e56d67d6ef74eeec6654e4adf8a143dc90
chientrm <chientrm@gmail.com>
2022-11-14 06:52:03 +0000

    init edge project

--- ui/src/lib/Label.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let props: {
-		caption: string;
-	};
-</script>
-
-<span>{props.caption}</span>
--- ui/src/lib/index.js
+++
@@ -1,5 +0,0 @@
-// Reexport your entry components here
-
-import Label from './Label.svelte';
-
-export { Label }
