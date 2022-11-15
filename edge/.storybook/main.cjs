155d40c057176accfe2f1acfdc755bdc342eb91e
chientrm <chientrm@gmail.com>
2022-11-15 05:47:19 +0000

    fix: svelte csf plugin

--- edge/.storybook/main.cjs
+++ edge/.storybook/main.cjs
@@ -1,20 +1,48 @@
-const preprocess = require('svelte-preprocess')
+const { loadConfigFromFile, mergeConfig } = require('vite');
+const path = require('path')
 
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+const config = {
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-svelte-csf'
   ],
   framework: '@storybook/svelte',
   core: {
     builder: '@storybook/builder-vite'
   },
-  svelteOptions: {
-    preprocess: [preprocess({ postcss: true })],
-  },
   features: {
-    storyStoreV7: true
+    // On-demand store does not work for .svelte stories, only CSF.
+    // Requires all stories to be loaded in bulk.
+    // REFERENCE https://storybook.js.org/docs/svelte/configure/overview#feature-flags
+    storyStoreV7: false,
+    // use own babel config instead of storybook provided one
+    babelModeV7: true
+  },
+  async viteFinal(config, { configType }) {
+    const userConfig = await loadConfigFromFile(
+      { command: 'build', mode: process.env.NODE_ENV || 'development' },
+      path.resolve(__dirname, '../vite.config.js')
+    );
+    // return the customized config
+    return mergeConfig(config, {
+      ...userConfig,
+      // customize the Vite config here
+      resolve: {
+        alias: {
+          $lib: path.resolve(__dirname, '../src/lib'),
+          //  when using typesafe-i18n
+          $i18n: path.resolve(__dirname, '../src/i18n'),
+          $app: path.resolve(__dirname, './sveltekit-mocks/app/')
+        }
+      }
+    });
   }
 };
+
+module.exports = config; 
---
+++ edge/src/stories/Button.stories.svelte
@@ -0,0 +1,5 @@
+<script lang='ts'>
+ 
+import {Meta, Template, Story} from "@storybook/addon-svelte-csf" 
+import Button from '$lib/components/Button.svelte' 
+</script>
