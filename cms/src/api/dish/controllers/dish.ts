5006e060646613242a129faf6159fb66adf629c0
chientrm <chientrm@gmail.com>
2022-11-24 11:24:13 +0000

    fix

--- cms/src/api/dish/controllers/dish.ts
+++ cms/src/api/dish/controllers/dish.ts
@@ -2,6 +2,6 @@
  * dish controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::dish.dish');
